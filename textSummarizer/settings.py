5abd31db0ed253a1d09c206edfa088e376ed404b
Divyansh <divyansh.sharma@hsbc.co.in>
2021-03-17 14:45:22 +0530

    changes

--- textSummarizer/settings.py
+++ textSummarizer/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k!#67@po&lmzfs+1-#8q5w&6k(uiy+qbgdbpj=07_)&56@8(7f'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
